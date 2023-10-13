17ee5b049493b5b1efd8ec65c9dc57a6fff4c6ee
Abhishek-kumar41 <127575574+AbhishekAK411@users.noreply.github.com>
2023-10-13 11:12:02 +0530

    updates

--- client/src/Components/Layout/ExperienceLayout/Experience.jsx
+++ client/src/Components/Layout/ExperienceLayout/Experience.jsx
@@ -1,8 +1,17 @@
 const Experience = () => {
     return (
         <>
-            <section className="mt-10 w-full h-[900px] border-black border">
-                
+            <section className="mt-10 w-full border-black border">
+                <section className=" py-5 w-full h-[500px] flex justify-evenly">
+                    <section className="w-[48%] h-full border-black border">
+                        <img src="" alt="" />
+                    </section>
+                    <section className="w-[48%] h-full border-black border"></section>
+                </section>
+                <section className="py-5 w-full h-[500px] flex justify-evenly">
+                    <section className="w-[48%] h-full border-black border"></section>
+                    <section className="w-[48%] h-full border-black border"></section>
+                </section>
             </section>
         </>
     )
